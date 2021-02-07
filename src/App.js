46004980b0fe51031c6a181deb04bac121430529
ThanhLeJP <thanhle1213@gmail.com>
2021-02-07 14:34:45 +0900

    commit 2

--- src/App.js
+++ src/App.js
@@ -4,21 +4,36 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
+        Boboテスト 
+        <br/>
+        遅延通知メール登録
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Form-container">
+          <form action="">
+            出発駅
+            <input type="text" name="出発駅" id=""/>
+            <br/>
+            到着駅
+            <input type="text" name="到着駅" id="" />
+            <br />
+            メール
+            <input type="text" name="" id="" />
+            <br />
+            <div class="Submit-button-container"><input type="submit" value="通知の受け取りを登録！" /></div>
+            
+          </form>
+        </div>
+
+
       </header>
     </div>
+
+
+
+    
+
   );
 }
 
